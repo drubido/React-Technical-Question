be3b2ece3c73f69b36f13652d4df28b669dc52e5
drubido <cdrr930725@gmail.com>
2023-05-16 18:35:50 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Data Grid Fro Technical Interview</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/costcode_budget_data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        costcode_number: "FM54DJJC",
+        costcode_name: "Electricity",
+        costcode_projected_money_budget: 15000,
+        costcode_hours: 39,
+        costcode_money_expend: 4000,
+    },
+    {
+        costcode_number: "YH6789JC",
+        costcode_name: "Plumming",
+        costcode_projected_money_budget: 8000,
+        costcode_hours: 165,
+        costcode_money_expend: 7500,
+    },
+    {
+        costcode_number: "FM6345JC",
+        costcode_name: "Paint",
+        costcode_projected_money_budget: 10000,
+        costcode_hours: 80,
+        costcode_money_expend: 5000,
+    },
+    {
+        costcode_number: "FM79HHJC",
+        costcode_name: "Roofing",
+        costcode_projected_money_budget: 75000,
+        costcode_hours: 540,
+        costcode_money_expend: 72000,
+    },
+    {
+        costcode_number: "PP89DJJC",
+        costcode_name: "Cement",
+        costcode_projected_money_budget: 45000,
+        costcode_hours: 67,
+        costcode_money_expend: 5000,
+    },
+    {
+        costcode_number: "FM5455KK",
+        costcode_name: "Carpintery",
+        costcode_projected_money_budget: 27000,
+        costcode_hours: 192,
+        costcode_money_expend: 16200,
+    }
+]
